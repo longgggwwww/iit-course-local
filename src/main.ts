2e3c4b1492bad966d41c339985c91a35090eece7
DANH PHI LONG <longsf9@gmail.com>
2023-11-02 09:48:47 +0700

    remove join func

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,12 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 import { json, static as static_ } from 'express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.setGlobalPrefix('api');
-  app.use('/uploads', static_(join('upload')));
+  app.use('/uploads', static_('upload'));
   app.enableCors();
   app.use(json({ limit: '200mb' }));
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
